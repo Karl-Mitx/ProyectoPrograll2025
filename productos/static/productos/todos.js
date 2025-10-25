6024e2972bd8164de6eaafd37c63175a1a71104a
Cmunoz <cmunozm6@miumg.edu.gt>
2025-10-24 23:11:35 -0600

    funcion para la peticion POST

--- productos/static/productos/todos.js
+++ productos/static/productos/todos.js
@@ -155,3 +155,12 @@ checkoutBtn?.addEventListener('click', () => {
     }
   });
 });
+function getCSRFToken() {
+  const name = 'csrftoken';
+  const cookies = document.cookie.split(';');
+  for (let cookie of cookies) {
+    const [key, value] = cookie.trim().split('=');
+    if (key === name) return decodeURIComponent(value);
+  }
+  return '';
+}
